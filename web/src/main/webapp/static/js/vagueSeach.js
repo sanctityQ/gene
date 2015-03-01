3653bac5cd3dce09334be2b1c92bb7b4e2ddb5d1
huabg2014 <huabaoguo@163.com>
2015-03-01 13:38:29 +0800

    客户姓名魔化查询，下拉增加小键盘选择

--- web/src/main/webapp/static/js/vagueSeach.js
+++ web/src/main/webapp/static/js/vagueSeach.js
@@ -13,10 +13,30 @@ function seachChange(url){
     var list = $("#seachList");
     list.css({'left':left,'top':top});
     list.on("click",'li',seachLiSelect);
-    seach.bind('input',function(){
+    seach.bind('keydown',function(event){
+        var select = $('li.selected',list);
+        if(event.keyCode == 40){
+            if(select.length){
+                if(select.index()!=$('li',list).length-1){
+                    select.removeClass('selected').next().addClass('selected');
+                }
+            }else{
+                $('li:first',list).addClass('selected');
+            }
+        }else if(event.keyCode == 38){
+            if(select.length){
+                if(select.index()!=0){
+                    select.removeClass('selected').prev().addClass('selected');
+                }
+            }else{
+                $('li:first',list).addClass('selected');
+            }
+        }else if(event.keyCode == 13){
+            var val = select.text();
+            select.trigger('click');
+        }else{
         ajaxSeach();
-    }).bind('keyup',function(){
-        ajaxSeach();
+        }
     });
     function ajaxSeach(){
         setTimeout(function(){
@@ -46,8 +66,8 @@ function seachChange(url){
             }else{
                 list.hide(100);
             }
-        },500)
-    }
+        },500);
+    };
 }
 function seachLiSelect(){
     var seach = $("#seachCustom");
@@ -57,4 +77,4 @@ function seachLiSelect(){
     seach.val(val).attr("tagId",id);
     $("#customerCode").val(id);
     list.hide(100);
-}
+};
