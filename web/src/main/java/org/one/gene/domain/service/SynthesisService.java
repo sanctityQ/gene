6653f4568630d47a50cdc46a6708c29b0afb9c17
huabg2014 <huabaoguo@163.com>
2016-06-01 17:42:16 +0800

    调整测值公式
    原来：1200*OdTB/(导入系统的值*20)
    改为：1.2*OdTB/（导入系统的值/30）

--- web/src/main/java/org/one/gene/domain/service/SynthesisService.java
+++ web/src/main/java/org/one/gene/domain/service/SynthesisService.java
@@ -1197,7 +1197,7 @@ public class SynthesisService {
 					}
 					if (operationType.equals(PrimerStatusType.measure) && measureMap.get(holeNo) != null) {
 						BigDecimal measure = (BigDecimal)measureMap.get(holeNo);
-						productNo = new BigDecimal(1200).multiply(primerProduct.getOdTB()).divide(measure.multiply(new BigDecimal(20)),0, BigDecimal.ROUND_UP)+"";
+						productNo = new BigDecimal(1.2).multiply(primerProduct.getOdTB()).divide(measure.divide(new BigDecimal(30)),0, BigDecimal.ROUND_UP)+"";
 					}
 				}
 				
